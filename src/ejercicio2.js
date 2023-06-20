244d9814fe61a47c1b88f377a3119a5c9566afd1
Danna <gutierrezdanna119@gmail.com>
2023-06-20 15:45:01 -0600

    Cambios del dia

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,16 @@
-class Contador {
-  // Inserte el código aquí
+//1. Realice el ejercicio en el archivo: ejercicio1.js
+//2. Cree una clase con:
+//* Una propiedad valor inicializada en 0
+//* Un método siguiente que incremenente el valor en 1.
+//3. Defina una variable llamada "instancia" que inicialice una instancia de la clase.
+
+class numero {
+  valor = 0;
+  siguiente() {
+    this.valor += 1;
+    return this.valor;
+  }
 }
-
 // Inserte el código aquí
-
-export { instancia }
+const instancia = new numero();
+export { instancia };
