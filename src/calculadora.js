261731cd62e74874545db646648688ccbb19a373
Danna <gutierrezdanna119@gmail.com>
2023-06-23 13:07:33 -0600

    Archivo correcto

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,21 +1,3 @@
-// var express = require("express");
-// var cors = require("cors");
-
-// var app = express();
-
-// app.use(cors())
-
-// app.listen(3000, () => {
-//   console.log("Server running on port 3000");
-// });
-
-// app.get("/users", (req, res, next) => {
-//   res.json([
-//     { id: 1, name: "Maria", birth: 1990 },
-//     { id: 2, name: "Luis", birth: 1980 },
-//   ]);
-// });
-
 function suma(numero1, numero2) {
   return numero1 + numero2;
 }
