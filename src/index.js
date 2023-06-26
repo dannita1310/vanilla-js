5feae32af6ea8a916078d9529f5215997367c3c6
Danna <gutierrezdanna119@gmail.com>
2023-06-26 08:03:18 -0600

    Trabajo finalizado reloj

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-class animal {
-  nombre = "";
-  constructor(nombre) {
-    this.nombre = nombre;
-  }
+let hour = document.getElementById("hrValue");
+let min = document.getElementById("minValue");
+let sec = document.getElementById("secValue");
+let ampm = document.getElementById("ampm");
 
-  saludo() {
-    console.log("Hola, el animal se llama", this.nombre);
-  }
-}
+const updateTime = () => {
+  let time = new Date();
+  let h = time.getHours();
+  let m = time.getMinutes();
+  let s = time.getSeconds();
+  min.innerText = m < 10 ? "0" + m : m;
+  sec.innerText = s < 10 ? "0" + s : s;
 
-const animal1 = new animal("Unicornio");
+  if (h < 12) {
+    ampm.innerText = "AM";
+  } else {
+    ampm.innerText = "PM";
+    h = h % 12;
+  }
+  hour.innerText = h < 10 ? "0" + h : h;
+  setInterval(updateTime, 1000);
+};
+updateTime();
